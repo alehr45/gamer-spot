b610249fb1602cea73ee3b6fe8ba1e95cbaa9605
zakonik <zakonik2417@gmail.com>
2021-03-30 17:27:49 -0400

    edited handlebars files, incomplete

--- server.js
+++ server.js
@@ -5,13 +5,19 @@ const hbs = exphbs.create({});
 const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require('path');
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.use(routes);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
