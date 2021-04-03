2281c6c6f8b14c07c7f0fff9c287867898a2649b
fthompson24 <fraserjthompson@gmail.com>
2021-04-02 21:02:42 -0500

    rawg api - merch seeds

--- public/javascript/api.js
+++ public/javascript/api.js
@@ -1,22 +1,23 @@
+ 
+ 
  function pullData(data) {
-   var apiKey = "c05709b8272a44a0924883e7b3228b14"
+  var apiKey = "c05709b8272a44a0924883e7b3228b14";
   var apiUrl = "https://api.rawg.io/api/games?key=" + apiKey;
-      fetch(apiUrl)
-        .then(function (response) {
-          if (response.ok) {
-            response.json().then(function (data) {
-             showGames(data);
-             
-            });
-          }
+    fetch(apiUrl)
+      .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          showGames(data);
         });
-    
-    };
+      }
+  });
+
+};
 
   
 function showGames(data) {
       var searchUrl1 =  data.results[0].sourceUrl;
-      document.getElementById('game1').innerHTML = data.results[0].name;
+      document.getElementById('game-details').src = data.results[0].background_image;
 console.log(searchUrl1)
   };
 
