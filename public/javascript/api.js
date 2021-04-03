cea9078b9e30343a47ac673ab963cd1996c13624
fthompson24 <fraserjthompson@gmail.com>
2021-04-02 23:50:46 -0500

    fetched game data from rawg api

--- public/javascript/api.js
+++ public/javascript/api.js
@@ -1,8 +1,10 @@
  
  
  function pullData(data) {
+  var game = document.getElementById('name').innerHTML.split('-');
+  game = game[0].trim().replace(/\W+/g, '-').toLowerCase();
   var apiKey = "c05709b8272a44a0924883e7b3228b14";
-  var apiUrl = "https://api.rawg.io/api/games?key=" + apiKey;
+  var apiUrl = "https://api.rawg.io/api/games/" + game + "?key=" + apiKey;
     fetch(apiUrl)
       .then(function (response) {
       if (response.ok) {
@@ -16,9 +18,11 @@
 
   
 function showGames(data) {
-      var searchUrl1 =  data.results[0].sourceUrl;
-      document.getElementById('game-details').src = data.results[0].background_image;
-console.log(searchUrl1)
+      //var searchUrl1 =  data.results.sourceUrl;
+      document.getElementById('game-pic').src = data.background_image;
+      document.getElementById('secondary-pic').src = data.background_image_additional;
+      document.getElementById('description').innerHTML = data.description_raw;
+console.log(data.rating);
   };
 
 
