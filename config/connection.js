b35af01749e40e993db7d5f84a73621c74113be7
Your NameAlex <alehr45@gmail.com>
2021-04-04 22:35:30 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+require('dotenv').config();
+
+// create connection to our db
+let sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // create connection to our database, pass in your MySQL info
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+      port: 3306
+  });
+}
 
 module.exports = sequelize;
