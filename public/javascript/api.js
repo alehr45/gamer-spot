f078f43c3fd707dced36aa3e04f05c4a3ddd5ba7
fthompson24 <fraserjthompson@gmail.com>
2021-04-04 19:38:55 -0500

    working on rawg

--- public/javascript/api.js
+++ public/javascript/api.js
@@ -1,28 +1,31 @@
- 
- 
- function pullData(data) {
-  var game = document.getElementById('name').innerHTML.split('-');
-  game = game[0].trim().replace(/\W+/g, '-').toLowerCase();
-  var apiKey = "c05709b8272a44a0924883e7b3228b14";
-  var apiUrl = "https://api.rawg.io/api/games/" + game + "?key=" + apiKey;
-    fetch(apiUrl)
-      .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          showGames(data);
-        });
-      }
-  });
-
+function pullData(data) {
+  var game = document.getElementsByClassName('name');
+  // let evenNumbers = game.filter(number => number % 2 == 0)
+  for (i=0; i<game.length; i++) {
+    gameName = game[0].innerText.replace("'", "").replace(/\W+/g, '-').toLowerCase();
+    //console.log(gameName);
+    searchData(gameName)
+  }
+  function searchData(gameName) {
+    var apiKey = "c05709b8272a44a0924883e7b3228b14";
+    var apiUrl = "https://api.rawg.io/api/games/" + gameName + "?key=" + apiKey;
+      fetch(apiUrl)
+        .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            showGames(data);
+          });
+        }
+    });
+  }
 };
 
   
 function showGames(data) {
-      //var searchUrl1 =  data.results.sourceUrl;
+      console.log(data.name);
       document.getElementById('game-pic').src = data.background_image;
-      document.getElementById('secondary-pic').src = data.background_image_additional;
       document.getElementById('description').innerHTML = data.description_raw;
-console.log(data.rating);
+      //console.log(data.rating);
   };
 
 
